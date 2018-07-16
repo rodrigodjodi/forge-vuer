d59ab6648743c04c0fa59c624dc47347e6f28b50
rodrigodjodi <rodrigo.djodi@gmail.com>
2018-07-16 18:25:16 -0300

    first function deploy

---
+++ functions/config.js
@@ -0,0 +1,15 @@
+"use strict";
+module.exports = {
+  credentials: {
+    client_id: "KiBrx6pRDrpUVB4ELA3zJj3PAh99REug",
+    client_secret: "Tx1eeUPGeCCt9P7g"
+  },
+  scopeInternal: [
+    "bucket:create",
+    "bucket:read",
+    "data:read",
+    "data:create",
+    "data:write"
+  ],
+  scopePublic: ["viewables:read"]
+};
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,9 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.credentials = functions.https.onRequest((request, response) => {
+  response.json({
+    access_token:
+      "eyJhbGciOiJIUzI1NiIsImtpZCI6Imp3dF9zeW1tZXRyaWNfa2V5In0.eyJjbGllbnRfaWQiOiJLaUJyeDZwUkRycFVWQjRFTEEzekpqM1BBaDk5UkV1ZyIsImV4cCI6MTUzMTc3ODQ5MSwic2NvcGUiOlsidmlld2FibGVzOnJlYWQiXSwiYXVkIjoiaHR0cHM6Ly9hdXRvZGVzay5jb20vYXVkL2p3dGV4cDYwIiwianRpIjoiMDhkbFlFcDRMYUVZaTh1b1o3VWZpSDVLbk1pbE1JUjVnMlRhcVA5NDVxWVhvSGRpdXVxbGVNZWhEVGlOSGR5ZyJ9.0OxpVdfczaSTFIEUJmsG4a6U9_IbGBVFZO5N-uqv1FM",
+    expires_in: 3599
+  });
+});
